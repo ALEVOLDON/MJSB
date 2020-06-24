5e564c69ea550cdeda28b794cc8dd8d543e78561
Vladimir Rybalsky <alevoldon@gmail.com>
2020-06-24 14:01:36 +0300

    Using The Console End

--- Section 02 JavaScript Language Fundamentals/06. Using The Console/js_sandbox/app.js
+++ Section 02 JavaScript Language Fundamentals/06. Using The Console/js_sandbox/app.js
@@ -4,7 +4,26 @@ console.log(123);
 console.log(true);
 var greeting = 'Hello';
 console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
 
+console.error("This is some error");
+console.clear();
+console.warn("This is a warning");
+
+console.time("time");
+	console.log('10');
+	console.log('9');
+	console.log('8');
+	console.log('7');
+	console.log('6');
+	console.log('5');
+	console.log('4');
+	console.log('3');
+	console.log('2');
+	console.log('1');					
+console.timeEnd("time");
 /*
 	mult
 	line
