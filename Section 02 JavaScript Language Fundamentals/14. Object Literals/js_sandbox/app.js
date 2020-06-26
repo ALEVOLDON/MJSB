dcc19b4136e7dcb758e730e06babc8f0483a5fc9
Vladimir Rybalsky <alevoldon@gmail.com>
2020-06-26 22:33:24 +0300

    update files

--- Section 02 JavaScript Language Fundamentals/14. Object Literals/js_sandbox/app.js
+++ Section 02 JavaScript Language Fundamentals/14. Object Literals/js_sandbox/app.js
@@ -1,7 +1,7 @@
 const person = {
 	firstName: "Steve",
 	lastName: "Smith",
-	age: 30,
+	age: 36,
 	email: "steve@aol.com",
 	hobbies: ["music","sports"],
 	address: {
@@ -9,7 +9,7 @@ const person = {
 		state: "FL"
 	},
 	getBirtYear: function(){
-		return 1987;
+		return 2017 - this.age;
 	}
 }
 
@@ -20,6 +20,19 @@ val = person;
 val = person.firstName;
 val = person["lastName"];
 val = person.age;
-val = person.hobbies;
+val = person.hobbies[1];
+val = person.address.state;
+val = person.address["city"];
+val = person.getBirtYear();
 
 console.log(val);
+
+const people = [
+	{name: "John", age: 30},
+	{name: "Mike", age: 23},
+	{name: "Nancy", age: 40} 
+];
+
+for(let i = 0; i < people.length; i++){
+	console.log(people[i].age);
+}
