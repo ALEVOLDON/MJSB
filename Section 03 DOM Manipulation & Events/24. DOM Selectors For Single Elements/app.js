ca14a47c0331b42225190657f44d101ce3abf2e2
Vladimir Rybalsky <alevoldon@gmail.com>
2020-08-07 14:27:09 +0300

    update files

--- Section 03 DOM Manipulation & Events/24. DOM Selectors For Single Elements/app.js
+++ Section 03 DOM Manipulation & Events/24. DOM Selectors For Single Elements/app.js
@@ -1,23 +1,23 @@
 // document.getElementById()
 
-// console.log(document.getElementById('task-title'));
+console.log(document.getElementById('task-title'));
 
-// // Get things from the element
-// console.log(document.getElementById('task-title').id);
-// console.log(document.getElementById('task-title').className);
+// Get things from the element
+console.log(document.getElementById('task-title').id);
+console.log(document.getElementById('task-title').className);
 
-// const taskTitle = document.getElementById('task-title');
+const taskTitle = document.getElementById('task-title');
 
-// // Change styling
-// taskTitle.style.background = '#333';
-// taskTitle.style.color = '#fff';
-// taskTitle.style.padding = '5px';
-// // taskTitle.style.display = 'none';
+// Change styling
+taskTitle.style.background = '#333';
+taskTitle.style.color = '#fff';
+taskTitle.style.padding = '5px';
+// taskTitle.style.display = 'none';
 
-// // Change content
-// taskTitle.textContent = 'Task List';
-// taskTitle.innerText = 'My Tasks';
-// taskTitle.innerHTML = '<span style="color:red">Task List</span>';
+// Change content
+taskTitle.textContent = 'Task List';
+taskTitle.innerText = 'My Tasks';
+taskTitle.innerHTML = '<span style="color:red">Task List</span>';
 
 // document.querySelector()
 
